14306d914f8ee8295188956a6e345d716d5fa43a
Deon Lobo <deonvictorlobo@gmail.com>
2024-12-30 21:54:25 -0500

    add redux toolkit

--- deo-estate-server/controllers/auth.controller.js
+++ deo-estate-server/controllers/auth.controller.js
@@ -19,9 +19,10 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return errorHandler(404, "User not found");
+    console.log(validUser);
+    if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return errorHandler(401, "Wrong Credentials");
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: hashedPassword, ...rest } = validUser._doc;
     res
