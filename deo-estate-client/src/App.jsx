5536a9c7e223bd87ef39cf253c8897e7005e034e
Deon Lobo <deonvictorlobo@gmail.com>
2025-01-01 18:25:19 -0500

    update header and make profile page privete

--- deo-estate-client/src/App.jsx
+++ deo-estate-client/src/App.jsx
@@ -1,22 +1,24 @@
-
-import { BrowserRouter , Routes , Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/about"
-import Profile from "./pages/Profile"
-import SignUp from "./pages/SignUp"
-import SignIn from "./pages/Signin"
-import Header from "./components/header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/about";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/Signin";
+import Header from "./components/header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
