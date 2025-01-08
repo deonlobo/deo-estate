89771b2e4c599755045528b59cc47c6c0b0dce0b
Deon Lobo <deonvictorlobo@gmail.com>
2025-01-07 22:24:04 -0500

    update user be

--- deo-estate-server/controllers/user.controller.js
+++ deo-estate-server/controllers/user.controller.js
@@ -1,3 +1,37 @@
+import bcrypt from "bcryptjs";
+import { set } from "mongoose";
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
   res.json({ message: "API route is working" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(403, "You cna only update your own account"));
+  }
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updatedUser._doc;
+
+    return res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
